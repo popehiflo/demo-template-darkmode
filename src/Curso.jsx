e40f8503d4a3ae8cdaff16434e446dd66a54428b
popehiflo <poolpetterhijuel@hotmail.com>
2020-02-21 06:59:18 -0500

    validacion de la propiedad en todos los sitios donde lo usa el componente

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -7,11 +7,13 @@ import React from 'react';
 const Curso = ({title, image, price, profesor}) => (
     <article className="card">
       <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-        <img src={image ? image : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRPxvoy0hvOiEurmgGwx6VExV1MDqDJPDwJZS_AlRtm5IqLxBew"} alt={title} />
+        <img 
+          src={image ? image : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRPxvoy0hvOiEurmgGwx6VExV1MDqDJPDwJZS_AlRtm5IqLxBew"} 
+          alt={title ? title : "No hay alt para esta imagen"} />
       </div>
       <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
         <h3 className="center">
-          {title}
+          {title ? title : "No hay Titulo!"}
         </h3>
         <div className="s-main-center">
           <div className="card__teacher s-cross-center">
