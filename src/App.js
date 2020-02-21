6c60251a5fef9021b5ec23fc2a3021deb839a3d2
popehiflo <poolpetterhijuel@hotmail.com>
2020-02-20 20:17:50 -0500

    primer commit del proyecto de catalogo de cursos hecho en react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles/styles.scss";
+import Curso from "./Curso";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+<>
+  <div className="main-banner img-container l-section" id="main-banner">
+    <div className="ed-grid lg-grid-6">
+      <div className="lg-cols-4 lg-x-2">
+        <img className="main-banner__img" alt="banner" src="https://eloutput.com/app/uploads-eloutput.com/2019/03/sonic-real-imagen-pelicula.jpg"/>
+        <div className="main-banner__data s-center">
+          <p className="t2 s-mb-0">Catalogo de Cursos</p>
+          <p> Ingresa gratis a todos los cursos</p>
+          <a href="https://google.com.pe" className="button">Registrate</a>
+        </div>
+      </div>
     </div>
-  );
-}
+  </div>
+
+  <div className="ed-grid m-grid-3">
+    <Curso />
+    <Curso />
+    <Curso />
+    <Curso />
+    <Curso />
+    <Curso />
+    
+  </div>
+</>
+)
 
 export default App;
