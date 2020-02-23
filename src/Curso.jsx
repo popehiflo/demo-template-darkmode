bc2ffc26087c51e37ac5032edd3b2f6d6812d197
popehiflo <poolpetterhijuel@hotmail.com>
2020-02-23 08:47:56 -0500

    quitando validaciones de propiedades por defecto para usar propTypes y defaultProps, indico el tipo y valor por defecto de cada propiedad recibida

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 //props es el objeto donde se almacenan las propiedades que yo puse en App.js
 //Para no recibir todo el objeto props, puedo solo recibir las llaves que necesito
@@ -7,14 +8,10 @@ import React from 'react';
 const Curso = ({title, image, price, profesor}) => (
     <article className="card">
       <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-        <img 
-          src={image ? image : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRPxvoy0hvOiEurmgGwx6VExV1MDqDJPDwJZS_AlRtm5IqLxBew"} 
-          alt={title ? title : "No hay alt para esta imagen"} />
+        <img src={image} alt={title} />
       </div>
       <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-        <h3 className="center">
-          {title ? title : "No hay Titulo!"}
-        </h3>
+        <h3 className="center">{title}</h3>
         <div className="s-main-center">
           <div className="card__teacher s-cross-center">
             <div className="card__avatar s-mr-1">
@@ -22,7 +19,7 @@ const Curso = ({title, image, price, profesor}) => (
                 <img src="path/img" alt="" />
               </div>
             </div>
-            <span className="small">{`Prof.: ${profesor}`}</span>
+            <span className="small">{profesor}</span>
           </div>
         </div>
         <div className="s-main-center">
@@ -31,5 +28,19 @@ const Curso = ({title, image, price, profesor}) => (
       </div>
     </article>
 )
+/*Definiendo que tipo de dato esperamos en cada propiedad que llega a curso */
+Curso.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.string,
+  profesor: PropTypes.string
+}
+/*Definiendo que valor por defecto debe tener en cada propiedad*/
+Curso.defaultProps = {
+  title: "No se encontro Titulo!",
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRPxvoy0hvOiEurmgGwx6VExV1MDqDJPDwJZS_AlRtm5IqLxBew",
+  price: "--",
+  profesor: ""
+}
 
 export default Curso;
